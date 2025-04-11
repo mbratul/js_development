bebf69b30f85d4cf9b06f3b88df22e488153964e
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-04-11 11:49:42 +0600

    update javascript file

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("hello world");
+
+let first_name = "Muhtasim Billah";
+let last_name = "Ratul";
+
+let full_name = first_name +" "+ last_name ;
+
+console.log(full_name);
+console.log("this is a test display");
+console.log(50+50+50);
+
+
