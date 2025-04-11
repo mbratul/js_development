60ff9bc128d231586f10469f616134c6eabf532f
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-04-11 12:13:05 +0600

    add js file

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ let full_name = first_name +" "+ last_name ;
 
 console.log(full_name);
 console.log("this is a test display");
-console.log(50+50+50);
+console.log(50+50+50+50+50);
+let space_remove = "        hello world         ";
+console.log(space_remove.trim());
+console.log(space_remove.split(" "));
+console.log(space_remove.trim());
 
 
