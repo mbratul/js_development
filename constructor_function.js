06f10eea61eef9375efea9af487bf9e968384761
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-05-18 15:11:25 +0600

    constructor function describe here

---
+++ constructor_function.js
@@ -0,0 +1,8 @@
+/*  JavaScript Function() Constructor
+
+    definition: The JavaScript Function() constructor is used to create new function objects dynamically. By using the Function() constructor with the new operator, developers can define functions on the fly, passing the function body as a string. This allows for greater flexibility in situations where functions need to be created based on runtime conditions or user input.
+
+    One notable characteristic of functions created with the Function() constructor is that they have a global scope, meaning they are accessible from anywhere in the code.
+    
+    url: https://www.geeksforgeeks.org/javascript-function-constructor/
+ */
