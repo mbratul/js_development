ccd6cfc3236dc4ba67463a68beb3584ef6509d9c
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-11-17 21:19:20 +0600

    learning promise structure in async js

---
+++ async_promise.js
@@ -0,0 +1,40 @@
+// promise flow control
+/**
+ * a promisee has two option
+ * 1 resolve means (a successful promise)
+ * 2 reject means (a failed promis)
+ */
+const hasMeeting = false;
+
+/**
+ * basic structure of define a promise
+ */
+const promise = new Promise((resolve, reject) => {
+  if (!hasMeeting) {
+    const meetingDetails = {
+      title: "technical meeting",
+      location: "google meet",
+      time: "09:00 AM",
+    };
+    resolve(meetingDetails);
+  } else {
+    reject(new Error("meeting already scheduled"));
+  }
+});
+
+const addToCalender = (meetingDetails) => {
+  const calender = `${meetingDetails.title} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+  //   return new Promise((resolve) => {
+  //     resolve(calender);
+  //   });
+  return Promise.resolve(calender);
+};
+
+promise
+  .then((meetingDetails) => {
+    console.log(JSON.stringify(meetingDetails));
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+console.log(`lets see what happened to me`);
