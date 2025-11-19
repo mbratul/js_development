a182d07246d380adf671a0f9f7c1dd1171bfc263
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-11-19 22:49:31 +0600

    learning addd, update, delete in js using fetch api

--- fetch_api/books/script.js
+++ fetch_api/books/script.js
@@ -1,8 +1,12 @@
+/**
+ * get data from my own json server
+ * @returns {string}
+ */
+const url = "http://localhost:3000/books";
 async function fetchBooks() {
-  const url = "http://localhost:3000/books";
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+
   const bookListEl = document.querySelector("#book-list");
   const showList = data
     .map((book) => {
@@ -13,4 +17,44 @@ async function fetchBooks() {
 
   bookListEl.innerHTML = showList;
 }
+
+async function addBook() {
+  const title = prompt("Enter book name");
+  const author = prompt("Enter author name");
+  //   console.log(JSON.stringify({ title, author }));
+  await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({ title, author }),
+  });
+  fetchBooks();
+}
+
+async function updateBook() {
+  await fetch(`${url}/1`, {
+    method: "PUT",
+    body: JSON.stringify({
+      title: "Refactoring",
+      author: "Martin Fowler",
+    }),
+  });
+  fetchBooks();
+}
+
+async function patchBook() {
+  await fetch(`${url}/1`, {
+    method: "PATCH",
+    body: JSON.stringify({
+      title: "Refactoring -XYZ",
+    }),
+  });
+  fetchBooks();
+}
+
+async function deleteBook() {
+  await fetch(`${url}/3d03`, {
+    method: "DELETE",
+  });
+  fetchBooks();
+}
+
 fetchBooks();
