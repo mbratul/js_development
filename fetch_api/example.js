37ff73ca71ad7cc4226b71ddd3066b2d993709a8
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-11-19 07:07:45 +0600

    learning fetch api workflow

--- fetch_api/example.js
+++ fetch_api/example.js
@@ -2,10 +2,21 @@
 
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
-fetch(url)
+/* fetch(url)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     console.log(data);
-  });
+  }); */
+
+/* const response = await fetch(url);
+const data = await response.json();
+console.log(data); */
+
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+}
+fetchData();
--- fetch_api/script.js
+++ fetch_api/script.js
@@ -1,2 +1,30 @@
-// api workflow in js
-console.log("hello ");
+// fetch api workflow in js
+
+async function doSomething(params) {
+  /* fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then((response) => response.json())
+    .then((data) => {
+      const result = document.querySelector("#result");
+      result.innerHTML = `
+        <h2 class="text-2xl">${data.title}</h2>
+        <p>${data.body}</p>
+      `;
+    }); */
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await response.json();
+  const result = document.querySelector("#result");
+  result.innerHTML = `
+        <h2 class="text-2xl">${data.title}</h2>
+        <p>${data.body}</p>
+      `;
+}
+
+async function showDogs() {
+  const response = await fetch("https://dog.ceo/api/breeds/image/random");
+  const data = await response.json();
+  const result = document.querySelector("#result");
+  result.innerHTML = `   
+        <h2 class="text-2xl">${data.message}</h2>     
+        <img src="${data.message}" width="200" height="200" alt="">
+      `;
+}
