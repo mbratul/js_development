94c9022dd52f2a4f1b3926e58b5f6dfd68e8f69d
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-11-28 22:11:39 +0600

    learning oop and fetch data

--- oop_js/currency.js
+++ oop_js/currency.js
@@ -1,4 +1,26 @@
 class CurrencyConvert {
-  USDtoBDT() {}
-  BDTtoUSD() {}
+  static async fetchRate() {
+    const response = await fetch(
+      "https://api.exchangerate-api.com/v4/latest/USD"
+    );
+    const data = await response.json();
+    return data.rates.BDT;
+  }
+  static async USDtoBDT(amount) {
+    const rate = await CurrencyConvert.fetchRate();
+    const usd = amount * rate;
+    return usd;
+  }
+  static async BDTtoUSD(amount) {
+    const rate = await CurrencyConvert.fetchRate();
+    const bdt = amount / rate;
+    return bdt;
+  }
 }
+const usdAmount = 100;
+const bdRate = await CurrencyConvert.USDtoBDT(usdAmount);
+console.log(`${usdAmount} USD is equal to ${bdRate} BDT`);
+
+const bdtAmount = 10000;
+const usdRate = await CurrencyConvert.BDTtoUSD(bdtAmount);
+console.log(`${bdtAmount} BDT is equal to ${usdRate} USD`);
