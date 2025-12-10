575fdf3669d9c5fa944aa5c3b29a01e8f841a044
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-12-10 17:01:08 +0600

    learning advance js concepts

--- advance_js_with_asief_mahir/01_class/js/script.js
+++ advance_js_with_asief_mahir/01_class/js/script.js
@@ -18,3 +18,27 @@ const friend = me;
 friend.age = 27;
 console.log(`Friend ${friend.age}`);
 console.log(`Me ${me.age}`);
+/** call stack example */
+function addition(a) {
+  return a + multiply(10);
+}
+function multiply(b) {
+  const c = 20;
+  return c * b;
+}
+const value = addition(10);
+console.log(value);
+
+/** closure example */
+function something() {
+  let a = 10;
+  let arr = [1, 2];
+  return function (b) {
+    return a * b * arr[1];
+  };
+}
+const result = something();
+const finalResult = result(100);
+console.log(finalResult);
+// console.dir(result);
+// console.log(something());
