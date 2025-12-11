df19fbcfa540a8345620897ad7d0702fda560732
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-12-11 11:17:02 +0600

    update some js file in 01_class

--- advance_js_with_asief_mahir/01_class/js/script.js
+++ advance_js_with_asief_mahir/01_class/js/script.js
@@ -1,44 +1,46 @@
 "use strict";
-let firstName = "muhtasim";
-let middleName = "billah";
-let lastName = "ratul";
+{
+  let firstName = "muhtasim";
+  let middleName = "billah";
+  let lastName = "ratul";
 
-let fullName = firstName + middleName + lastName;
-console.log(fullName);
+  let fullName = firstName + middleName + lastName;
+  console.log(fullName);
 
-var myName = "ratul";
-// console.log(window.myName); // global scope do not use
-// console.log(window.firstName); // functional scope that's why can not find out the window object
+  var myName = "ratul";
+  // console.log(window.myName); // global scope do not use
+  // console.log(window.firstName); // functional scope that's why can not find out the window object
 
-const me = {
-  fullName: "ratul",
-  age: 30,
-};
-const friend = me;
-friend.age = 27;
-console.log(`Friend ${friend.age}`);
-console.log(`Me ${me.age}`);
-/** call stack example */
-function addition(a) {
-  return a + multiply(10);
-}
-function multiply(b) {
-  const c = 20;
-  return c * b;
-}
-const value = addition(10);
-console.log(value);
-
-/** closure example */
-function something() {
-  let a = 10;
-  let arr = [1, 2];
-  return function (b) {
-    return a * b * arr[1];
+  const me = {
+    fullName: "ratul",
+    age: 30,
   };
+  const friend = me;
+  friend.age = 27;
+  console.log(`Friend ${friend.age}`);
+  console.log(`Me ${me.age}`);
+  /** call stack example */
+  function addition(a) {
+    return a + multiply(10);
+  }
+  function multiply(b) {
+    const c = 20;
+    return c * b;
+  }
+  const value = addition(10);
+  console.log(value);
+
+  /** closure example */
+  function something() {
+    let a = 10;
+    let arr = [1, 2];
+    return function (b) {
+      return a * b * arr[1];
+    };
+  }
+  const result = something();
+  const finalResult = result(100);
+  console.log(finalResult);
+  console.dir(result);
+  // console.log(something());
 }
-const result = something();
-const finalResult = result(100);
-console.log(finalResult);
-// console.dir(result);
-// console.log(something());
